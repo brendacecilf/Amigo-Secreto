519e2fce109c58b2b363af035f91d5d5bd84b9e2
brendacecilf <brendacecilf@gmail.com>
2025-03-18 22:54:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Arreglo para almacenar los nombres de amigos
 
-// Arreglo para almacenar los nombres de los amigos
 const amigos = [];
 
 // Función para agregar un amigo
